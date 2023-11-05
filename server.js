36f3db97d5ad04c3e396a8b75de8169d6adf2d01
MartaCanino <martacaninoromero@gmail.com>
2023-11-05 17:19:10 +0100

    Informacion cada 5 segundos

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const http = require('http');
-
-const hostname = '127.0.0.1';
-
-const port = 3000;
+const os = require('os');
+const process = require('process');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Node.js Server</h1>');
+    res.write('<h2>System Information:</h2>');
+    res.write('<div id="system-info"></div>');
+  }
 });
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+
+server.listen(3000, 'localhost', () => {
+  console.log('Node.js server is running at http://localhost:3000');
+  console.log(`Node.js version: ${process.version}`);
+  
+  setInterval(() => {
+    const systemInfo = {
+      cpuUsage: (os.cpus()[0].times.user / os.cpus()[0].times.sys).toFixed(2),
+      memoryUsage: ((os.totalmem() - os.freemem()) / os.totalmem() * 100).toFixed(2),
+      systemUptime: os.uptime(),
+      nodeUptime: process.uptime(),
+    };
+    
+    const systemInfoString = JSON.stringify(systemInfo);
+    
+    // Send system info to the client
+    server.getConnections((err, count) => {
+      if (count > 0) {
+        server.clients.forEach(client => {
+          client.send(`data: ${systemInfoString}\n\n`);
+        });
+      }
+    });
+  }, 5000); // Send system info every 5 seconds
 });
